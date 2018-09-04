a8fd85709b3d02ebe90b2820b5f8fe58c66039ae
Danfei Xu <morphine1021@gmail.com>
2018-09-03 22:03:41 -0700

    more doc, author header

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
+"""
+Author: Danfei Xu
+danfei@stanford.edu
+"""
+
+
 import os
-import sys
 import time
 import re
 from slackclient import SlackClient
 import arxiv
 
-import sumy
-from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
@@ -20,10 +23,11 @@ slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 starterbot_id = None
 
 # constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+RTM_READ_DELAY = 1  # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 ARXIV_REGEX = r'(https?://[^\s]+[0-9]+)'
+USER_HANDLE = 'danfei'  # your username on slack
 
 # for sumy
 lang = 'english'
@@ -80,13 +84,12 @@ def format_arxiv(article, do_summarize=True):
 
 
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
+    """Parses a list of events coming from the Slack RTM API to find bot commands.
+    If a bot command is found, this function returns a tuple of command and
+    channel. If its not found, then this function returns None, None.
     """
     for event in slack_events:
-        if event["type"] == "message" and not "subtype" in event:
+        if event["type"] == "message" and "subtype" not in event:
             user_id, message = parse_direct_mention(event["text"])
             if user_id == starterbot_id:
                 return message, event["channel"]
@@ -94,18 +97,21 @@ def parse_bot_commands(slack_events):
 
 
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """ Finds a direct mention (a mention that is at the beginning) in message text
+    and returns the user ID which was mentioned. If there is no direct mention,
+    returns None.
     """
     matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+    # the first group contains the username, the second group
+    # contains the remaining message
+    if matches:
+        return (matches.group(1), matches.group(2).strip())
+    else:
+        return (None, None)
 
 
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
+    """ Executes bot command if the command is known
     """
     # This is where you start to implement more commands!
     try:
@@ -118,7 +124,7 @@ def handle_command(command, channel):
         else:
             response = "Don't seem to find an arXiv link..."
     except:
-        response = 'Some exception caught. @danfei go debug!'
+        response = 'Some exception caught. @$s go debug!' % USER_HANDLE
 
     # Sends the response back to the channel
     slack_client.api_call(
